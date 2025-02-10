e186abbf01b708b4582065e9583bbb02b356b403
RaulDiaz <rulhdiaz@gmail.com>
2025-02-10 09:51:55 -0600

    actualizacion de cors frontend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 dotenv.config(); // Se cargan variables de entorno
 
 const corsOptions = {
-   origin: ['http://localhost:3000','https://accesodb.onrender.com'],
+   origin: ['http://localhost:3000','https://accesodb.onrender.com', "https://accesodb-frontend.onrender.com"],
    credentials: true,
    methods: ['GET', 'POST'],
    allowHeaders: ['Content-Type', 'Authorization']
