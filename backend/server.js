5a54127bf5df8743dc228404b3e2614a55285a92
RaulDiaz <rulhdiaz@gmail.com>
2025-02-10 12:24:54 -0600

    pruebas login

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,6 @@ sequelize.sync()
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
    console.log(`Server running on port ${PORT}`);
 })
