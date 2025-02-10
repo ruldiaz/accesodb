dd00bdbbba7970aa771b8a08f0131ce0bb54f3b7
ruldiaz <rulhdiaz@gmail.com>
2025-02-09 23:09:23 -0600

    archivos docker y base de datos en neon

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -1,9 +1,28 @@
 const { Sequelize } = require('sequelize');
+require("dotenv").config();
 
+//const sql = neon(process.env.DATABASE_URL);
+
+/*
 const sequelize = new Sequelize(
    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/accesodb`,{
    logging: false,
    native: false
 })
+*/
+const sequelize = new Sequelize(
+   process.env.DATABASE_URL,{
+   logging: false,
+   native: false
+})
+
+
 
 module.exports = sequelize;
+
+
+
+
+
+
+
