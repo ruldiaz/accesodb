f35c69dab2cd17cc5cbdf5f88dbbc3e797b139c7
RaulDiaz <rulhdiaz@gmail.com>
2025-02-11 12:55:42 -0600

    validacion correo

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -33,10 +33,10 @@ router.put('/update', authenticateToken, async (req, res) => {
       return res.status(401).json({message: "Usuario no encontrado."})
     }
 
-    if(user.email !== email){
+    if(user.email !== email.toLowerCase()){
       const existingEmailUser = await User.findOne({where: {email}});
  
-      if(existingEmailUser.id !== user.id){
+      if(existingEmailUser && existingEmailUser.id !== user.id){
         return res.status(400).json({message: "El correo que ingresaste no está disponible."});
       }
     }
@@ -45,9 +45,14 @@ router.put('/update', authenticateToken, async (req, res) => {
 
     return res.json({message: "Usuario actualizado correctamente.", user});
   } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      return res.status(400).json({ message: "El correo ya está registrado." });
+    }
+    
     console.error(error);
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: "Hubo un problema al actualizar el usuario." });
   }
+  
 })
 
 // Ruta con proteccion
